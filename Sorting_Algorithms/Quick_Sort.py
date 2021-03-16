9345a320275270fe3465337663b95cca8e86336d
duxuan1 <xuand.du@mail.utoronto.ca>
2021-03-16 11:30:09 -0400

    Update Quick_Sort.py

--- Sorting_Algorithms/Quick_Sort.py
+++ Sorting_Algorithms/Quick_Sort.py
@@ -1,32 +1,34 @@
 from typing import List, Tuple
+import random
 
 
-def quick_sort(lst: List[int]) -> List[int]:
+def quick_sort(lst: List[int], left, right):
     """
     Return a sorted list with the same elements as <lst>.
     break a list up (partition) into the part smaller than some
     value (pivot) and not smaller than that value, sort these parts, then
     recombine the list
     """
-    if len(lst) < 2:
-        return lst
-    pivot = lst[0]
-    smaller, bigger = partition(pivot, lst[1:])
-    return quick_sort(smaller) + [pivot] + quick_sort(bigger)
+    if left < right:
+        pivot = lst[right]
+        i = partition(pivot, lst, left, right - 1)
+        lst[i], lst[right] = lst[right], lst[i]
+        quick_sort(lst, left, i - 1)
+        quick_sort(lst, i + 1, right)
 
 
-def partition(pivot: int, lst: List[int]) -> Tuple[List, List]:
+def partition(pivot: int, lst: List[int], left: int, right: int):
     """
     break a list up (partition) into the part smaller than some
     value (pivot) and not smaller than that valu
     """
-    smaller, bigger = [], []
-    for i in range(len(lst)):
-        if lst[i] < pivot:
-            smaller.append(lst[i])
-        else:
-            bigger.append(lst[i])
-    return smaller, bigger
+    i = j = left
+    while j <= right:
+        if lst[j] < pivot:
+            lst[i], lst[j] = lst[j], lst[i]
+            i += 1
+        j += 1
+    return i
 
 
 def make_list(n: int) -> List[int]:
@@ -40,8 +42,9 @@ def make_list(n: int) -> List[int]:
 
 
 if __name__ == '__main__':
-    size = 1000
-    for i in range(4):
+    size = 5
+    for _ in range(1):
         L = make_list(size)
-        assert quick_sort(L) == sorted(L)
-        size *= 2
+        quick_sort(L, 0, len(L) - 1)
+        assert L == sorted(L)
+        size *= 5
